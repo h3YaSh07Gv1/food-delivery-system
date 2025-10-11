b76a067718f483e5b9a9ad3c11a511bbc1dbf348
h3YaSh07Gv1 <152728243+h3YaSh07Gv1@users.noreply.github.com>
2025-10-11 09:04:08 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const ADMIN_URL = process.env.NODE_ENV === "development"?"http://localhost:5174":"https://food-delivery-system-admin-ugml.onrender.com";
+export const ADMIN_URL = process.env.NODE_ENV === "development"?"http://localhost:5173":"https://food-delivery-system-5x3i.onrender.com";
